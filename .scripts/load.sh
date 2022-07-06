85fb9ecf946306d318108d8689dafe0002c985c3
Mvmo <mvmogo@gmail.com>
2022-07-06 23:38:40 +0200

    Fix loading of scripts and add aliases script

--- .scripts/load.sh
+++ .scripts/load.sh
@@ -1,25 +1,21 @@
-script_executable_path=$HOME/.scripts/exec/
-script_utility_path=$HOME/.scripts/util/
+script_executable_path=$HOME/.scripts/exec.d/
 
-## -- EXECUTABLE SCRIPTS -- ##
+# load all executable scripts in the exec.d directory to list them in the fuzzy finder and add an alias to them
 executable_scripts=$(find $script_executable_path -type f -name '*.sh' -not -name '*.sh.swp' | sed 's/^.*\///g' | sed 's/\.sh$//g')
 
 for script in $executable_scripts; do
+    echo $script
     alias "$(basename ${script%.sh})"="$script"
 done
 
 s() {
     to_execute=$(echo $executable_scripts | fzf)
-    bash $HOME/.scripts/exec/$to_execute.sh
+    bash $script_executable_path$to_execute.sh
 }
 
-## -- EXECUTABLE SCRIPTS -- ##
-
-## -- UTILITY SCRIPTS -- ##
-
-utility_scripts=$(find $script_utility_path -type f -name '*.sh' -not -name '*.sh.swp')
-for script in $utility_scripts; do
-    source $script
-done
-
-## -- UTILITY SCRIPTS -- ##
+# source all utility scripts
+if [ -d $HOME/.scripts/util.d ]; then
+  for file in $HOME/.scripts/util.d/*.sh; do
+    source $file
+  done
+fi
