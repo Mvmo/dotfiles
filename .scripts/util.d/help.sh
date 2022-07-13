49895b00b4b5f81e2a9905a5524e11c772f2c3d6
Mvmo <mvmogo@gmail.com>
2022-07-13 23:22:06 +0200

    Add fzf-brew help

---
+++ .scripts/util.d/help.sh
@@ -0,0 +1,11 @@
+function helps() {
+    echo "Fuzzy Brew: fbh"
+}
+
+function fbh() {
+    echo "Fuzzy Brew: "
+    echo "fbi: Fuzzy brew install"
+    echo "fbui: Fuzzy brew uninstall"
+    echo "fci: Fuzzy cask install"
+    echo "fcui: Fuzzy cask uninstall"
+}
