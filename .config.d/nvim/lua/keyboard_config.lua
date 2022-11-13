94f6606a5464aaa2e10e089e1af57a87653f00d1
Mvmo <mvmogo@gmail.com>
2022-11-13 19:39:47 +0100

    Telescope find hidden files

--- .config.d/nvim/lua/keyboard_config.lua
+++ .config.d/nvim/lua/keyboard_config.lua
@@ -10,7 +10,7 @@ vim.cmd [[
     nmap <Left> :wincmd h<cr>
     nmap <Right> :wincmd l<cr>
 
-    nnoremap <leader>ff <cmd>Telescope find_files<cr>
+    nnoremap <leader>ff <cmd>Telescope find_files hidden=true<cr>
     nnoremap <leader>fg <cmd>Telescope live_grep<cr>
     nnoremap <leader>fb <cmd>Telescope buffers<cr>
     nnoremap <leader>fh <cmd>Telescope help_tags<cr>
