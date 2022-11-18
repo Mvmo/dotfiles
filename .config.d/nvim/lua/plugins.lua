752fd0491914c342329a304c190bed1b422c3539
Mvmo <mvmogo@gmail.com>
2022-11-18 16:56:05 +0100

    Remove coc

--- .config.d/nvim/lua/plugins.lua
+++ .config.d/nvim/lua/plugins.lua
@@ -5,7 +5,7 @@ packadd packer.nvim
 return require("packer").startup(function()
     use "wbthomason/packer.nvim"
     use { "ellisonleao/gruvbox.nvim" }
-    use { "neoclide/coc.nvim", branch = "release" }
+
     use { "jiangmiao/auto-pairs" }
     use { "jparise/vim-graphql" }
     use { "nvim-lua/plenary.nvim" }
