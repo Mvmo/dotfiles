5133cad786b80b2e9097d3efdbec1b5077221a20
Mvmo <mvmogo@gmail.com>
2025-03-07 14:44:12 +0100

    update keymap and ignore pattern

--- .config.d/nvim/after/plugin/telescope.lua
+++ .config.d/nvim/after/plugin/telescope.lua
@@ -3,7 +3,7 @@ local builtin = require("telescope.builtin")
 
 telescope.setup({
     defaults = {
-        file_ignore_patterns = { "node_modules", ".next", ".git", ".dist" }
+        file_ignore_patterns = { "node_modules", ".next", ".git", ".dist", "target" }
     }
 })
 
@@ -11,6 +11,7 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fs", function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- fb buffers
